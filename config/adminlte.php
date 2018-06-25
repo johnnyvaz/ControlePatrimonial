e9e14c7fcce12f58d4e49320c81a2660025e5ce3
Johnny Vaz <johnnyjohnnyjohnnyvaz@gmail.com>
2018-06-25 00:58:37 -0300

    menu limpo

--- config/adminlte.php
+++ config/adminlte.php
@@ -142,58 +142,58 @@ return [
             'url'  => 'admin/settings',
             'icon' => 'lock',
         ],
-        [
-            'text'    => 'Multilevel',
-            'icon'    => 'share',
-            'submenu' => [
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'Level One',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'Level Two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'Level Two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        'Campos',
-        [
-            'text'       => 'Important',
-            'icon_color' => 'red',
-        ],
-        [
-            'text'       => 'Warning',
-            'icon_color' => 'yellow',
-        ],
-        [
-            'text'       => 'Information',
-            'icon_color' => 'aqua',
-        ],
-    ],
+    //     [
+    //         'text'    => 'Multilevel',
+    //         'icon'    => 'share',
+    //         'submenu' => [
+    //             [
+    //                 'text' => 'Level One',
+    //                 'url'  => '#',
+    //             ],
+    //             [
+    //                 'text'    => 'Level One',
+    //                 'url'     => '#',
+    //                 'submenu' => [
+    //                     [
+    //                         'text' => 'Level Two',
+    //                         'url'  => '#',
+    //                     ],
+    //                     [
+    //                         'text'    => 'Level Two',
+    //                         'url'     => '#',
+    //                         'submenu' => [
+    //                             [
+    //                                 'text' => 'Level Three',
+    //                                 'url'  => '#',
+    //                             ],
+    //                             [
+    //                                 'text' => 'Level Three',
+    //                                 'url'  => '#',
+    //                             ],
+    //                         ],
+    //                     ],
+    //                 ],
+    //             ],
+    //             [
+    //                 'text' => 'Level One',
+    //                 'url'  => '#',
+    //             ],
+    //         ],
+    //     ],
+    //     'Campos',
+    //     [
+    //         'text'       => 'Important',
+    //         'icon_color' => 'red',
+    //     ],
+    //     [
+    //         'text'       => 'Warning',
+    //         'icon_color' => 'yellow',
+    //     ],
+    //     [
+    //         'text'       => 'Information',
+    //         'icon_color' => 'aqua',
+    //     ],
+     ],
 
     /*
     |--------------------------------------------------------------------------
